d1befcb2b3c3f8b88217ab57d794bee59a01b633
plan <sputnik3555@gmail.com>
2025-03-02 02:27:01 +0300

    first commit

--- проход.py
+++ проход.py
@@ -212,7 +212,8 @@ def upload_process():
 
 
         # Инициализируйте драйвер
-        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+        driver = webdriver.Firefox(options=options)
+
         # driver.maximize_window()
         # time.sleep(20000)
 
