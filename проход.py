30260e82b00f5f09eb5a9281f64d9fe84abd8a38
plan <sputnik3555@gmail.com>
2025-03-02 02:39:19 +0300

    first commit

--- проход.py
+++ проход.py
@@ -190,7 +190,6 @@ def upload_process():
         from selenium import webdriver
         from selenium.webdriver.firefox.options import Options
         from fake_useragent import UserAgent
-        from webdriver_manager.firefox import GeckoDriverManager
         ua = UserAgent()
         random_user_agent = ua.random
         options = Options()
